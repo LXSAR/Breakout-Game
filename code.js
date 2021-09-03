ebcd33cf2be928f4a15b1e9f5aa19e940943bb72
LXSAR <89995140+LXSAR@users.noreply.github.com>
2021-09-03 13:03:17 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,195 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var box1 = createSprite(25, 75, 50, 50);
+box1.shapeColor="red";
+var box2 = createSprite(75, 75, 50, 50);
+box2.shapeColor="blue";
+var box3 = createSprite(125, 75, 50, 50);
+box3.shapeColor="red";
+var box4 = createSprite(175, 75, 50, 50);
+box4.shapeColor="blue";
+var box5 = createSprite(225, 75, 50, 50);
+box5.shapeColor="red";
+var box6 = createSprite(275, 75, 50, 50);
+box6.shapeColor="blue";
+var box7 = createSprite(325, 75, 50, 50);
+box7.shapeColor="red";
+var box8 = createSprite(375, 75, 50, 50);
+box8.shapeColor="blue";
+
+var box9 = createSprite(25, 125, 50, 50);
+box9.shapeColor="blue";
+var box10 = createSprite(75, 125, 50, 50);
+box10.shapeColor="red";
+var box11 = createSprite(125, 125, 50, 50);
+box11.shapeColor="blue";
+var box12 = createSprite(175, 125, 50, 50);
+box12.shapeColor="red";
+var box13 = createSprite(225,125, 50, 50);
+box13.shapeColor="blue";
+var box14 = createSprite(275, 125, 50, 50);
+box14.shapeColor="red";
+var box15 = createSprite(325, 125, 50, 50);
+box15.shapeColor="blue";
+var box16 = createSprite(375, 125, 50, 50);
+box16.shapeColor="red";
+
+var score = 0;
+var gamestate = "start";
+var paddle=createSprite(200,390,100,20);
+var ball=createSprite(200,200,20,20);
+
+function draw() {
+  background("white");
+  fill("black");
+  strokeWeight(3);
+  stroke("red");
+  textFont("copperplate");
+  textSize(25);
+  text("Score = "+score, 180, 20);
+  textSize(20);
+
+  if(gamestate=="start"){
+  text("Welcome! Please press enter to start", 70, 180); 
+  if (keyDown("enter")) {
+  ball.setVelocity(4, 5);
+  gamestate = "play";
+  }
+  }
+  if(gamestate=="play"){
+   paddle.x = World.mouseX;
+  if(score== 16 || ball.isTouching(bottomEdge)){
+    gamestate= "end";
+  }
+  }
+  if(gamestate=="end"){
+  text("Game Over! Score is "+score, 100, 200);
+  ball.setVelocity(0, 0);
+  }
+  createEdgeSprites();
+  ball.bounceOff(topEdge);
+  ball.bounceOff(rightEdge);
+  ball.bounceOff(leftEdge);
+  ball.bounceOff(paddle);
+  if (ball.isTouching(box1)) {
+    box1.destroy();
+    score++;
+  }
+  if (ball.isTouching(box2)) {
+    box2.destroy();
+    score++;
+  }
+if (ball.isTouching(box3)) {
+    box3.destroy();
+    score++;
+  }
+  if (ball.isTouching(box4)) {
+    box4.destroy();
+    score++;
+  }
+  if (ball.isTouching(box5)) {
+    box5.destroy();
+    score++;
+  }
+  if (ball.isTouching(box6)) {
+    box6.destroy();
+    score++;
+  }
+  if (ball.isTouching(box7)) {
+    box7.destroy();
+    score++;
+  }
+  if (ball.isTouching(box8)) {
+    box8.destroy();
+    score++;
+  }
+  if (ball.isTouching(box9)) {
+    box9.destroy();
+    score++;
+  }
+  if (ball.isTouching(box10)) {
+    box10.destroy();
+    score++;
+  }
+if (ball.isTouching(box11)) {
+    box11.destroy();
+    score++;
+  }
+if (ball.isTouching(box12)) {
+    box12.destroy();
+    score++;
+  }
+  if (ball.isTouching(box13)) {
+    box13.destroy();
+    score++;
+  }
+  if (ball.isTouching(box14)) {
+    box14.destroy();
+    score++;
+  }
+  if (ball.isTouching(box15)) {
+    box15.destroy();
+    score++;
+  }
+  if (ball.isTouching(box16)) {
+    box16.destroy();
+    score++;
+  }
+  drawSprites();
+}
+
+
+
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
